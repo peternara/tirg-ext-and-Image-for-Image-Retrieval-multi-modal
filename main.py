77d4466a2cee70230e0863f6873cfdbd8b07454d
peternara <peternara@naver.com>
2020-05-18 14:29:12 +0000

    first : 20200518-1

--- main.py
+++ main.py
@@ -266,6 +266,9 @@ def main():
   #  logger.add_text(k, str(opt.__dict__[k]))
 
   trainset, testset = load_dataset(opt)
+  for t in trainset.get_all_texts():
+      print(t)
+
   model, optimizer = create_model_and_optimizer(
       opt, [t.decode('utf-8') for t in trainset.get_all_texts()])
 
